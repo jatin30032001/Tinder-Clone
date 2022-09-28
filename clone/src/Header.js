da313f58f326dceaf4fc46984cf35cb2237d8461
jatin30032001 <jrsaini2090@gmail.com>
2022-09-28 18:47:41 +0530

    Header Commit

---
+++ clone/src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+import Person2Icon from '@mui/icons-material/Person2';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+import logo from './logo.png' 
+
+const Header = () => {
+  return (
+    <div className='header'>
+       
+
+       <IconButton>
+      <Person2Icon fontSize='large' className='header_icon'/>
+      </IconButton>
+      
+<img className='header_logo' src={logo} alt="Tinder Logo" />
+
+<IconButton>
+<ForumIcon/>
+</IconButton>
+    </div>
+  )
+}
+
+export default Header
