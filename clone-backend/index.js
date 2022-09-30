33c3fa93517939336385ac2c232bc61709dbfce2
jatin30032001 <jrsaini2090@gmail.com>
2022-09-30 13:08:03 +0530

    Backend and Databases

--- clone-backend/index.js
+++ clone-backend/index.js
@@ -1,8 +1,52 @@
 
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
+import Cards from "./dbCrads.js";
 
 
 const app = express();
 
-const port = process.env.PORT
+const port = process.env.PORT || 8001
+
+const connection_url = `mongodb+srv://Jatin:Jatin@123@cluster0.qnn5s1m.mongodb.net/?retryWrites=true&w=majority`
+
+ mongoose.connect(connection_url,{
+    useNewUrlParser :true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+ })
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Heloo World')
+})
+
+app.post('/tinder/cards', (req,res)=>{
+     const dbCard = req.body;
+
+     Cards.create(dbCard,(err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(201).send(data);
+        }
+     })
+});
+
+app.get('/tinder/cards', (req,res)=>{
+    Cards.find((err,data)=>{
+       if(err){
+           res.status(500).send(err);
+       }
+       else{
+           res.status(200).send(data);
+       }
+    })
+});
+
+
+
+app.listen(port ,()=> {
+    console.log(`listening on localhost:${port}`)
+})
